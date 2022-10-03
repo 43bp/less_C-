8adff4498d33030fa797886b48294e7610904f49
43bp2 <43bp@mail.ru>
2022-10-03 22:38:19 +0300

    add all less2

---
+++ LessCSharp2/Task2.0/Program.cs
@@ -0,0 +1,27 @@
+﻿// Найти максимальное из 9 чисел
+int Max(int arg1, int arg2, int arg3) // пишем метод поиска максимума из трех
+{
+int result = arg1;
+if (arg2 > result) result = arg2;
+if (arg3 > result) result = arg3;
+return result;
+}
+
+int a1 = 15;
+int a2 = 55;
+int a3 = 12;
+int a4 = 13;
+int a5 = 2;
+int a6 = -2;
+int a7 = 0;
+int a8 = 65;
+int a9 = 12;
+
+int max1 = Max(a1, a2, a3);   // сравниваем спомощью нашего метода по три числа
+int max2 = Max(a4, a5, a6);
+int max3 = Max(a7, a8, a9);
+int max = Max(max1, max2, max3);  // потом из тройки ищем максимальное
+// а можно и так:
+// int max = Max(Max(a1, a2, a3),Max(a4, a5, a6),Max(a7, a8, a9))
+
+Console.WriteLine(max);
---
+++ LessCSharp2/Task2.1array/Program.cs
@@ -0,0 +1,15 @@
+﻿// Найти максимальное из 9 чисел
+int Max(int arg1, int arg2, int arg3) // пишем метод поиска максимума из трех
+{
+int result = arg1;
+if (arg2 > result) result = arg2;
+if (arg3 > result) result = arg3;
+return result;
+}
+
+int[] array = {11,22,3,4,52,36,0,80,9}; // вводим массив
+
+int max = Max(Max(array [0],array [1],array [2]), 
+              Max(array [3],array [4],array [5]),
+              Max(array [6],array [7],array [8])); // также ищем максимальное по трем
+Console.WriteLine(max);
