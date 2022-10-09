22569ea2fde74582fbeb293432bc67dac6654de9
43bp2 <43bp@mail.ru>
2022-10-09 20:49:56 +0300

    add tasks less3

---
+++ LessCSharp3/Task3.1/Program.cs
@@ -0,0 +1,37 @@
+﻿// Дан текст
+// В тексте заменить все пробелы черточками
+// Маленькие "к" заменить большими "К"
+// Большие "С" заменить маленькими "с"
+
+string text = "- Я думаю, - сказал князь, улыбаясь, что, "
+            + "ежели бы вас послали вместо нашего милого Винценгероде,"
+            + "вы бы взяли приступом согласе прусского короля."
+            + "Вы так красноречивы. Вы дадите мне чаю?";
+
+// string s = "qwerty"
+//             012345
+// s [3]  // r
+
+string Replace(string text, char oldValue, char newValue)
+{
+    string result = String.Empty; // инициализация пустой строки
+    int length = text.Length;
+    for (int i = 0; i < length; i++)
+    {
+    if (text[i] == oldValue) result = result + $"{newValue}";
+    else result = result + $"{text[i]}";
+    }
+
+    return result;
+}
+
+string newText = Replace(text, ' ', '|'); // в одинарные кавычки заключаются char (символы)
+Console.WriteLine(newText);
+Console.WriteLine();
+
+newText = Replace(newText, 'к', 'К'); // в одинарные кавычки заключаются char (символы)
+Console.WriteLine(newText);
+Console.WriteLine();
+
+newText = Replace(newText, 'С', 'с'); // в одинарные кавычки заключаются char (символы)
+Console.WriteLine(newText);
---
+++ LessCSharp3/Task3.2/Program.cs
@@ -0,0 +1,37 @@
+﻿// Упорядочивание массива
+// Метод выбора минимального (максимального)
+
+int[] arr = {1, 3, 9, 5, 8, 2, 1, 3, 6};
+
+void PrintArray(int[] array)  // напишем сначала метод печати массива
+{
+    int count = array.Length;
+    for (int i = 0; i < count; i++)
+    {
+Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+
+void SelectionSort(int[] array) // напишем метод для сортировки с меньшего к большему
+{
+    for (int i = 0; i < array.Length; i++) // задаем цикл до конца массива (почти до конца, так как сравниваем левый элемент с правым), здесь сразу используем метод Length
+    {
+        int minPos = i;  //вводим переменную с индексом минимального элемента
+       
+        for (int j = i + 1; j < array.Length; j++) // задаем цикл до конца массива с соседним элементом для сравнения (j = i + 1 )
+        {
+            if(array[j] < array[minPos]) minPos = j; // если элемент j меньше элемента minPos, то присваеваем minPos j.
+        }
+
+        int temporary = array [i];  // меняем элементы местами. запоминаем наш элемент
+        array[i] = array[minPos]; // записываем в него элемент с минимальным значением
+        array[minPos] = temporary;  // а в элемент с мин.знач. записываем запомненный нами элемент
+
+    }
+}
+
+PrintArray(arr);
+SelectionSort(arr);
+Console.WriteLine();
+PrintArray(arr);
